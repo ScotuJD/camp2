680ac400f691368fbe48788692734e635c6500f6
ScotuJD <123363964+ScotuJD@users.noreply.github.com>
2024-07-31 18:51:33 +0200

    Add files via upload

---
+++ bootcamp2-chat/src/bootcamp_chat_frontend/src/App.vue
@@ -0,0 +1,128 @@
+<script lang="ts">
+import { ref } from 'vue';
+import { bootcamp_chat_backend, canisterId, createActor } from '../../declarations/bootcamp_chat_backend';
+import { AuthClient } from '@dfinity/auth-client';
+import { HttpAgent } from '@dfinity/agent';
+import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
+import type { UserData } from '../../declarations/bootcamp_chat_backend/bootcamp_chat_backend.did';
+
+export default {
+  data() {
+    return {
+      newChat: "",
+      chats: [] as string[][],
+      identity: undefined as undefined | Identity,
+      principal: undefined as undefined | Principal,
+      targetPrincipal: "",
+      userData: undefined as undefined | UserData,
+      newUsername: ""
+    }
+  },
+  methods: {
+    isUserLogged() {
+      if (!this.identity || !this.principal || this.principal === Principal.anonymous()) {
+        throw new Error("PLZ log in")
+      }
+      return {
+        identity: this.identity,
+        principal: this.principal 
+      }
+    },
+    validateTargetPrincipal(){
+      const cleanTargetPrincipal = this.targetPrincipal.trim();
+      if (cleanTargetPrincipal === ""){
+        throw new Error("No principal")
+      }
+      const targetPrincipal = Principal.fromText(cleanTargetPrincipal)
+      if (!targetPrincipal || targetPrincipal === Principal.anonymous()){
+        throw new Error("Wrong target")
+      }
+      return targetPrincipal
+    },
+    getAuthClient(){
+      this.isUserLogged()
+      return createActor(canisterId, {
+        agentOptions: {
+          identity: this.identity
+        }
+      });
+    },
+    async dodajChatMSG() {
+      const targetPrincipal = this.validateTargetPrincipal()
+      const backend = this.getAuthClient();
+      await backend.add_chat_msg(this.newChat, targetPrincipal)
+      await this.pobierzChaty()
+    },
+    async pobierzChaty() {
+      const {identity, principal} = this.isUserLogged()
+      const targetPrincipal = this.validateTargetPrincipal()
+
+      const chatPath = [targetPrincipal, identity.getPrincipal()].sort()
+      this.chats = await bootcamp_chat_backend.get_chat(chatPath)
+    },
+    async login() {
+      const authClient = await AuthClient.create();
+      await authClient.login({
+        identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/",
+        onSuccess: async () => {
+          const identity = authClient.getIdentity();
+          const principal = identity.getPrincipal();
+          this.principal = principal;
+          this.identity = identity;
+          console.log("Zalogowano", this.principal)
+          await this.getUserData()
+        }
+      })
+    },
+    async logout () {
+      const authClient = await AuthClient.create();
+      await authClient.logout()
+      this.identity = undefined;
+      this.principal = undefined;
+      this.chats = [];
+      this.userData = undefined
+    },
+    async registerUsername() {
+      const trimedUsername = this.newUsername.trim();
+      const backend = this.getAuthClient();
+      await backend.register(trimedUsername)
+      await this.getUserData()
+    },
+    async getUserData() {
+      const {principal} = this.isUserLogged()
+      const maybeUserData = await bootcamp_chat_backend.get_user(principal as Principal)
+      if (maybeUserData.length === 0) {
+        this.userData = undefined
+      } else {
+        this.userData = maybeUserData[0]
+      }
+      console.log("User data", this.userData)
+    }
+  },
+}
+</script>
+
+<template>
+  <main>
+    {{ principal }} 
+    <button v-if="!principal" @click="login">login</button>
+    <button v-if="principal" @click="logout">logout</button>
+    <div v-if="principal && !userData">
+      <input v-model="newUsername" /> <button @click="registerUsername"></button>
+    </div>
+    <div v-if="principal && userData">
+      <div>
+        <input v-model="targetPrincipal" /><button @click="pobierzChaty">pobierz chat</button>
+      </div>
+      <div>
+        <div v-for="chat in chats[0]">
+          {{ chat }}
+        </div>
+      </div>
+      <div>
+        <textarea v-model="newChat"></textarea><button @click="dodajChatMSG">Dodaj notatke</button>
+      </div>
+    </div>
+  </main>
+</template>
